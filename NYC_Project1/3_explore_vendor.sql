e25544ff149f50ab533f98a93c015d4ffe7e64cf
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-05-29 14:03:24 +0200

    3_explore_vendor.sql

--- NYC_Project1/3_explore_vendor.sql
+++ NYC_Project1/3_explore_vendor.sql
@@ -1,28 +1,31 @@
 --Read data using external data source from vendor.csv
+
+--without quatation in csv file in vendor_name Creative Mobile Technologies, LLC. Result - Creative Mobile Technologies 
 SELECT
     *
 FROM
     OPENROWSET(
         BULK 'vendor_unquoted.csv',
-		DATA_SOURCE = 'nyc_taxi_data',
+	DATA_SOURCE = 'nyc_taxi_data_raw',
         FORMAT = 'CSV',
         HEADER_ROW = TRUE,
-		PARSER_VERSION = '2.0',
+	PARSER_VERSION = '2.0',
         FIELDTERMINATOR=',',
         ROWTERMINATOR='\n',
-		FIELDQUOTE='"'
+	FIELDQUOTE='"'
     ) AS [result]
-	
+
+--with quatation in csv file in vendor_name "Creative Mobile Technologies, LLC". Result - Creative Mobile Technologies, LLC
 SELECT
     *
 FROM
     OPENROWSET(
         BULK 'vendor.csv',
-		DATA_SOURCE = 'nyc_taxi_data',
+	DATA_SOURCE = 'nyc_taxi_data',
         FORMAT = 'CSV',
         HEADER_ROW = TRUE,
-		PARSER_VERSION = '2.0',
+	PARSER_VERSION = '2.0',
         FIELDTERMINATOR=',',
         ROWTERMINATOR='\n',
-		FIELDQUOTE='"'
+	FIELDQUOTE='"'
     ) AS [result]
